3346f7c0b608aacaaee53d690de1b2c46346387b
Nahakiole <robin.glauser@gmail.com>
2014-06-28 12:06:51 +0200

    Updated DefaultController and Templates

--- src/Nahakiole/FlyBundle/Controller/DefaultController.php
+++ src/Nahakiole/FlyBundle/Controller/DefaultController.php
@@ -3,22 +3,81 @@
 namespace Nahakiole\FlyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+    private $adjectives = ['adorable', 'hilarious', 'famous', 'gracious', 'golden', 'gray', 'yellow', 'pink', 'blue', 'red', 'green', 'dreadful', 'striking', 'orange', 'charming', 'wild', 'purple', 'glorious', 'sparkling', 'Fast', 'Quick', 'Speedy', 'Swift', 'Hasty', 'Zippy', 'Rapid', 'Slow', 'Sluggish', 'Creeping', 'Dawdling', 'Meandering', 'Crawling', 'Beautiful', 'Striking', 'Stunning', 'Gorgeous', 'Picturesque', 'Lovely', 'Charming', 'Enchanting', 'Exquisite', 'Delicate', 'Ugly', 'Hideous', 'Horrid', 'Dreadful', 'Obnoxious', 'Nasty', 'Ghastly', 'Cruel', 'Revolting', 'Intimidating', 'Menacing', 'Miserable', 'Dangerous', 'Rude', 'Spoiled', 'Wild', 'Lazy', 'Selfish', 'Delinquent', 'Greedy', 'Vile', 'Ridiculous', 'Kind', 'Gentle', 'Quiet', 'Caring', 'Fair', 'Compassionate', 'Benevolent', 'Polite', 'Amusing', 'Generous', 'Entertaining', 'Hopeful', 'Lively', 'Creative', 'Brave', 'Good', 'Fantastic', 'Marvelous', 'Fabulous', 'Splendid', 'Brilliant', 'Superb', 'Dynamite', 'Bad', 'Dreadful', 'Terrible', 'Ghastly', 'Filthy', 'Repulsive', 'Awful', 'Happy', 'Joyful', 'Ecstatic', 'Cheerful', 'Delighted', 'Blithe', 'Carefree', 'Bored', 'Hardworking', 'Mysterious', 'Verbose', 'Laconic', 'Curious', 'Bucolic', 'Silly', 'Contrary', 'Shocking', 'Wild', 'Rambunctious', 'Courageous', 'Cowardly', 'Ornery', 'Gullible', 'Thrifty', 'Famous', 'Infamous', 'Brazen', 'Cold', 'Hard', 'Subtle', 'Gullible', 'Hungry', 'Anxious', 'Nervous', 'Antsy', 'Impatient', 'Shining', 'Crispy', 'Soaring', 'Endless', 'Sparkling', 'Fluttering', 'Spiky', 'Scrumptious', 'Eternal', 'Slimy', 'Slick', 'Gilded', 'Ancient', 'Smelly', 'Glowing', 'Rotten', 'Decrepit', 'Lousy', 'Grimy', 'Rusty', 'Sloppy', 'Muffled', 'Foul', 'Rancid', 'Fetid', 'Small', 'Itty-bitty', 'Tiny', 'Puny', 'Miniscule', 'Minute', 'Diminutive', 'Petite', 'Slight', 'Big', 'Huge', 'Gigantic', 'Monstrous', 'Immense', 'Great', 'Tremendous', 'Enormous', 'Massive', 'Whopping', 'Vast', 'Brawny', 'Hulking', 'Bulky', 'Towering', 'Hot', 'Steaming', 'Sweltering', 'Scorching', 'Blistering', 'Sizzling', 'Muggy', 'Stifling', 'Sultry', 'Oppressive', 'Cold', 'Chilly', 'Freezing', 'Icy', 'Frosty', 'Bitter', 'Arctic', 'Difficult', 'Demanding', 'Trying', 'Challenging', 'Easy', 'Simple', 'Effortless', 'Relaxed', 'Calm', 'Tranquil', 'Heavy', 'Serious', 'Grave', 'Profound', 'Intense', 'Severe'];
+
+    private $objects = ['pirate', 'chocolate', 'pegasus', 'unicorn', 'shark', 'ninja', 'zombie', 'sun', 'glass', 'dream', 'day', 'army', 'song', 'cow', 'power', 'library', 'product', 'area', 'society', 'theory', 'party', 'Crime', 'Cupcake', 'Oxygen', 'Push', 'Room', 'Slip', 'Swan', 'Word', 'Writer', 'Baker', 'Coil', 'Company', 'Garlic', 'Makeup', 'Mary', 'Sycamore', 'Twine', 'Wrecker', 'Age', 'Attic', 'Baseball', 'Bed', 'Cupcake', 'Policeman', 'Retailer', 'Tom-tom ', 'Ton'];
+
     public function indexAction()
     {
-        return $this->render('FlyBundle:Default:packages.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('\Nahakiole\FlyBundle\Entity\Application');
+        $applications = $repository->findAll();
+        return $this->render('FlyBundle:Default:packages.html.twig', ['applications' => $applications]);
     }
 
-    public function aboutAction(){
-
+    public function aboutAction()
+    {
         return $this->render('FlyBundle:Default:about.html.twig');
     }
 
 
-    public function faqAction(){
-
+    public function faqAction()
+    {
         return $this->render('FlyBundle:Default:faq.html.twig');
+    }
+
+    public function downloadAction()
+    {
+        $rawApplications = $this->get('request')->request->get('applications');
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('\Nahakiole\FlyBundle\Entity\Application');
+
+        $applicationsToInstall = $applications = [];
+        if (is_array($rawApplications)) {
+            foreach ($rawApplications as $id => $value) {
+                if ($value == 'on') {
+                    $applicationsToInstall[] = $id;
+                }
+            }
+        } else {
+            return new RedirectResponse($this->generateUrl('fly_homepage'));
+        }
+        $applications = $repository->findById($applicationsToInstall);
+        return $this->render('FlyBundle:Default:download.html.twig', ['applications' => $applications, 'packageName' => $this->createPackageName()]);
+    }
+
+    public function packageAction($Package)
+    {
+        return new Response('Content', Response::HTTP_OK, array('content-type' => 'application/octet-stream')); // the headers public attribute is a ResponseHeaderBagreturn $response;
+    }
+
+    /**
+     * @param Array $applicationId Array with the ids of the packages
+     * @return string Package name
+     */
+    private function createPackage($applicationId)
+    {
+
+
+    }
+
+    private function createPackageName()
+    {
+        $adjective = array_rand($this->adjectives);
+        $secondAdjective = array_rand($this->adjectives);
+        while ($secondAdjective == $adjective) {
+            $secondAdjective = array_rand($this->adjectives);
+        }
+        $object = array_rand($this->objects);
+        $secondObject = array_rand($this->objects);
+        while ($object == $secondObject) {
+            $secondObject = array_rand($this->objects);
+        }
+        return ucfirst($this->adjectives[$adjective]) . ucfirst($this->adjectives[$secondAdjective]) . ucfirst($this->objects[$object]) . ucfirst($this->objects[$secondObject]);
     }
 }
--- src/Nahakiole/FlyBundle/Resources/public/js/main.js
+++ src/Nahakiole/FlyBundle/Resources/public/js/main.js
@@ -1,28 +1,50 @@
-jQuery(function(){
-    jQuery('.btn-add-software').click(function(event){
+jQuery(function () {
+    var checkbox;
+    jQuery('.btn-add-software').click(function (event) {
         event.preventDefault();
-        if(jQuery(this).hasClass('btn-software-added')){
+        checkbox = jQuery(this).find('.application-checkbox');
+        checkbox.prop("checked", !checkbox.prop("checked"));
+
+        console.log(checkbox.prop("checked"));
+        if (jQuery(this).hasClass('btn-software-added')) {
             jQuery(this).removeClass('btn-software-added');
-            jQuery(this).html('Add');
+            jQuery(this).find('.action').html('Add');
 
         }
         else {
             jQuery(this).addClass('btn-software-added');
-            jQuery(this).html('Added');
+            jQuery(this).find('.action').html('Added');
         }
+
+        jQuery('.count-packages').html(jQuery(".application-checkbox:checked").length);
+
     });
 
-    jQuery('.start-building').click(function(){
+    jQuery('.start-building').click(function () {
         jQuery('html, body').animate({
-            scrollTop: jQuery(".container-hero").offset().top
+            scrollTop: jQuery(".container-hero").offset().top + 20
         }, 1337, 'easeInOutExpo');
     });
 
-    var packagesContainer = jQuery('.packages');
-// initialize
-    packagesContainer.masonry({
-        itemSelector: '.col-sm-6'
+
+    jQuery(".filter").keyup(function () {
+        // Retrieve the input field text and reset the count to zero
+        var filter = $(this).val(), count = 0;
+        // Loop through the comment list
+        $(".packages > .list-group").each(function(){
+
+            // If the list item does not contain the text phrase fade it out
+            if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+                $(this).fadeOut();
+
+                // Show the list item if the phrase matches and increase the count by 1
+            } else {
+                $(this).fadeIn();
+                count++;
+            }
+        });
     });
 
 
 });
+
