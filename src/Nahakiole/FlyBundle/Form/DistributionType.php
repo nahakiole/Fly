00d4ee39bdfe8e75584344e83053bddbfec80f48
Nahakiole <robin.glauser@gmail.com>
2014-09-06 20:39:08 +0200

    Added application and distribution crud.

---
+++ src/Nahakiole/FlyBundle/Form/DistributionType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Nahakiole\FlyBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DistributionType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('packetManager')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Nahakiole\FlyBundle\Entity\Distribution'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'nahakiole_flybundle_distribution';
+    }
+}
